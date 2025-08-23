cff10508020858378df2aa022db0be9e5eb4967d
Ishaan Sharma <sharmaishaan0009@gmail.com>
2025-08-24 00:58:59 +0530

    another concept

--- practice.java
+++ practice.java
@@ -30,10 +30,29 @@ public class practice {
             }
         }
     }
+
+    public static void RevArr(int arr[]) {
+      int first = 0;
+      int last = arr.length - 1;
+
+      while (first < last) {
+        int temp = arr[first];
+        arr[first] = arr[last];
+        arr[last] = temp;
+        first++;
+        last--;
+      }
+
+      for(int i = 0; i < arr.length; i++) {
+          System.out.print(arr[i] + " ");
+      }
+      System.out.println();
+    }
     public static void main(String[] args) {
         int arr[] = {2,4,6,8,10,12,14};
         int key = 10;
         Largest(arr);
         BinSearch(arr, key);
+        RevArr(arr);
     }
 }
