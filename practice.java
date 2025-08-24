64f4a9e5b8b99e6fe9956f48844ea9174a45f633
Ishaan Sharma <sharmaishaan0009@gmail.com>
2025-08-24 23:52:04 +0530

    revised another concept

--- practice.java
+++ practice.java
@@ -48,11 +48,22 @@ public class practice {
       }
       System.out.println();
     }
+
+    public static void Pairs(int arr[]) {
+        for(int i = 0; i < arr.length; i++){
+            for(int j = i + 1 ; j < arr.length; j++){
+                System.out.println(arr[i] +  "," + arr[j]);
+            }
+            System.out.println();
+        }
+       
+    }
     public static void main(String[] args) {
         int arr[] = {2,4,6,8,10,12,14};
         int key = 10;
         Largest(arr);
         BinSearch(arr, key);
         RevArr(arr);
+        Pairs(arr);
     }
 }
