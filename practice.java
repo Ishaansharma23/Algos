c71a6c7a0162b623bf992124e8d9e18dc0181816
Ishaan Sharma <sharmaishaan0009@gmail.com>
2025-08-26 23:54:31 +0530

    rev

--- practice.java
+++ practice.java
@@ -59,7 +59,7 @@ public class practice {
        
     }
 
-    public static void PrintSubArr(int arr[]) {
+    public static void PrintSubArr(int arr[]) { // Print SubArrays
         for(int i = 0; i < arr.length; i++){
             for(int j = i ; j < arr.length; j++){
                 for(int k = i; k <= j; k++){
