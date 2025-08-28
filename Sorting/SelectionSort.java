0a87332ef4dcfd8305bd0de90b508aa380c03e69
Ishaan Sharma <sharmaishaan0009@gmail.com>
2025-08-29 01:26:07 +0530

    .

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -5,34 +5,30 @@
 // Input =  {29, 10, 14, 37, 13};
 // Output = 10 13 14 29 37
 
-// LOGIC - smallest elemnt nikalo array mai s or fir compare kro current element s jonsa 
-// chota hoga wo age ajayega aise hi ek round ka krdo then fir swap krdo after the 1 round
-// 1 round complete krne pr swap hoga 
+// LOGIC - smallest elem nikalo adjacent s compare krke fir jo smallest aya usko swap krdo bahar wale loop k index s
 
-// (“Har round mein, unsorted part ka sabse chhota element dhoondo aur usse current 
-// position ke element ke saath swap karo.”)
 
-// "Select the smallest and place it at the front."
 
 package Sorting;
 
 public class SelectionSort {
     public static void Sort(int arr[]) {
-        for(int i = 0; i < arr.length - 1; i++) {
-            int minIndex = i;
-
-            // Find the index of the minimum element in the unsorted part
-            for(int j = i + 1; j < arr.length; j++) {
+        for(int i = 0; i < arr.length; i++){
+            int minIndex = i;   // asuming yehi sbse chota elem hai 
+            for(int j = i + 1; j < arr.length;j++){
                 if (arr[minIndex] > arr[j]) {
-                    minIndex = j;
+                    minIndex = j; // minindex ko update krdo
                 }
             }
-
-            // Swap the found minimum with the current element at index i
+    
+            // pehle min nikalo sbse loop laga kr fir swap krdo jonsa bahar wala idx ka loop hai usske sth
             int temp = arr[i];
             arr[i] = arr[minIndex];
             arr[minIndex] = temp;
-        }
+          }
+          for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+          }
     }
 
     public static void Print(int arr[]) {
