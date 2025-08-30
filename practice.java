8536bab02d7bed9a225d85a28e87e5fa29e9a4c8
Ishaan Sharma <sharmaishaan0009@gmail.com>
2025-08-30 15:38:11 +0530

    .

--- practice.java
+++ practice.java
@@ -218,7 +218,17 @@ public class practice {
    }
     
    public static void InsertionSort(int arr[]) {
-        
+
+    for(int i = 0; i < arr.length; i++){
+        int curr = arr[i];
+        int prev = i - 1;
+            while (arr[prev] > curr && prev >= 0) {
+                arr[prev + 1] = arr[prev];
+                prev--;
+            }
+            arr[prev + 1] = curr;
+    }
+
    }
 
     public static void main(String[] args) {
