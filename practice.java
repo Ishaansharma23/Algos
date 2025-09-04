b6b5362641e831d91f69c9aac6b643edf1fc6971
Ishaan Sharma <sharmaishaan0009@gmail.com>
2025-09-04 19:32:34 +0530

    .

--- practice.java
+++ practice.java
@@ -260,6 +260,10 @@ public class practice {
             }
         }
 
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+
     }
 
     public static void main(String[] args) {
